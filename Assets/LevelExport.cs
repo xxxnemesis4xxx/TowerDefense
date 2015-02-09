86c78620b928c84cbf0dc831d9d1f853df8ef0be
xxxnemesis4xxx <magnitude9@live.ca>
2015-02-09 14:18:51 -0500

    Custom Editor Part 2

--- Assets/LevelExport.cs
+++ Assets/LevelExport.cs
@@ -1,6 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEditor;
+using System.Xml.Linq;
+
 
 public class LevelExport : EditorWindow {
 
@@ -13,7 +15,7 @@ public class LevelExport : EditorWindow {
 	Vector2 scrollPosition = Vector2.zero;
 	int noOfEnemies;
 	int initialMoney;
-	int MinCarroSpawnTime, MaxCarrotSpawnTime;
+	int MinCarrotSpawnTime, MaxCarrotSpawnTime;
 	string filename;
 	int waypointsCount;
 	int pathsCount;
@@ -52,6 +54,77 @@ public class LevelExport : EditorWindow {
 		if (GUILayout.Button ("Export")) 
 		{
 			Export();
+		}
+	}
+
+	void Export()
+	{
+		//Create a new output filestream
+		doc = new XDocument ();
+		doc.Add (new XElement ("Elements"));
+		XElement elements = doc.Element ("Elements");
+
+		XElement pathPiecesXML = new XElement ("PathPieces");
+		var paths = GameObject.FindGameObjectWithTag ("Path");
+
+		foreach (var item in paths) {
+			XElement path = new XElement("Path");
+			XAttribute attrX = new XAttribute("X",item.transform.position.x);
+			XAttribute attrY = new XAttribute("Y",item.transform.position.y);
+			path.Add (attrX,attrY);
+			pathPiecesXML.Add (path);
+		}
+		pathsCount = paths.Length;
+		elements.Add (pathPiecesXML);
+
+		XElement waypointsXML = new XElement ("Waypoints");
+		var waypoints = GameObject.FindGameObjectsWithTag ("Waypoint");
+		if (!WaypointsAreValid(waypoints))
+		{
+			return;
+		}
+
+		//Order by user selected order
+		waypoints = waypoints.OrderBy (x => x.GetComponent<OrderedWaypointForEditor> ().Order).ToArray ();
+		foreach (var item in waypoints) {
+			XElement waypoint = new XElement ("Waypoint");
+			XAttribute attrX = new XAttribute ("X", item.transform.position.x);
+			XAttribute attrY = new XAttribute ("Y", item.transform.position.y);
+			waypoint.Add (attrX, attrY);
+			waypointsXML.Add (waypoint);
+		}
+		waypointsCount = waypoints.Length;
+		elements.Add (waypointsXML);
+
+		XElement roundsXML = new XElement ("Rounds");
+		foreach (var item in roundsXML) {
+			XElement round = new XElement ("Round");
+			XAttribute NoOfEnemies = new XAttributes ("NoOfEnemies", item.NoOfEnemies);
+			round.Add (NoOfEnemies);
+			roundsXML.Add (round);
+		}
+		elements.Add (roundsXML);
+
+		XElement towerXML = new XElement ("Tower");
+		var tower = GameObject.FindGameObjectWithTag ("Tower");
+		if (tower == null) {
+			ShowErrorForNull ("Tower");
+			return;
+		}
+		XAttribute towerX = new XAttribute ("X", tower.transform.position.x);
+		XAttribute towerY = new XAttribute ("Y", tower.transform.position.y);
+		towerXML.Add (towerX, towerY);
+		elements.Add (towerXML);
+
+		if (!InputIsValid ())
+			return;
+
+		if (EditorUtility.DisplayDialog ("Save confirmation",
+		                                "Are you sure you want to save level " + filename, "OK", "Cancel")) {
+			doc.Save ("Assets/" + filename);
+			EditorUtility.DisplayDialog ("Saved", filename + " saved!", "OK");
+		} else {
+			EditorUtility.DisplayDialog ("NOT Saved", filename + "not saved!", "OK");
 		}
 	}
 }
