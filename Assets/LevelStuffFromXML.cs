343354d573e83e384f5baaa427c6aa5cf849c450
xxxnemesis4xxx <magnitude9@live.ca>
2015-02-09 14:25:08 -0500

    Custom Editor Part 3

---
+++ Assets/LevelStuffFromXML.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Assets.Scripts
+{
+	public class LevelStuffFromXML
+	{
+		public float MinCarrotSpawnTime;
+		public float MaxCarrotSpawnTime;
+		public int InitialMoney;
+		public List<Round> Rounds;
+		public List<Vector2> Paths;
+		public List<Vector2> Waypoints;
+		public Vector2 Tower;
+		public LevelStuffFromXML()
+		{
+			Paths = new List<Vector2> ();
+			Waypoints = new List<Vector1> ();
+			Rounds = new List<Round> ();
+		}
+	}
+
+
+	public class Round 
+	{
+		public int NoOfEnemies { get; set; }
+	}
+}
